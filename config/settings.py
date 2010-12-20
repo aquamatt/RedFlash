90032f597fd9d8891031f00b5b83f0eadf5a8682
Matthew Pontefract <matthew.pontefract@glassesdirect.com>
2010-12-20 16:37:44 +0000

    added some installation instructions just in case someone checks out the first commit

--- config/settings.py
+++ config/settings.py
@@ -99,6 +99,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'templates')
 )
 
 INSTALLED_APPS = (
